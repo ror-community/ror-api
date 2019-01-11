33034b4be00f51fc9cfe69e6196a8ec4a507315c
Esha Datta <edatta@crossref.org>
2019-01-11 00:57:37 -0500

    with correct id syntax

--- bin/generate-id.rb
+++ bin/generate-id.rb
@@ -5,25 +5,25 @@ require 'base32/url'
 require 'pry'
 Bundler.require :default
 
-class GenerateId
-  def prefix
-    "ror.org"
+class RorID
+  def self.prefix
+    "https://ror.org"
   end
-  def upper_limit
+  def self.upper_limit
     200000000
   end
 
-  def construct_id
+  def self.construct
     prefix + "/" + encode_number
   end
 
-
-  private
-  def gen_number
+  def self.gen_number
     number = SecureRandom.random_number(upper_limit)
   end
 
-  def encode_number
-    Base32::URL.encode(gen_number, :split=>5, :length=>15)
+  def self.encode_number
+    Base32::URL.encode(gen_number, :checksum => true, :length=>8)
   end
+
+  private_class_method :prefix, :upper_limit, :gen_number, :encode_number
 end
