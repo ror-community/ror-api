4f69cec4871c9a575c139569d8e8c67ef2066292
Martin Fenner <martin.fenner@datacite.org>
2019-01-12 09:57:06 +0100

    simplify ssh config. #3

--- vendor/docker/10_ssh.sh
+++ vendor/docker/10_ssh.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 if [ "${PUBLIC_KEY}" ]; then
-  echo "${PUBLIC_KEY}" > /home/app/.ssh/authorized_keys
-  chmod 600 /home/app/.ssh/authorized_keys
+  echo "${PUBLIC_KEY}" > /root/.ssh/authorized_keys
 fi
