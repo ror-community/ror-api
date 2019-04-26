56409857764b18864ceca7e81e0d82d6887f7666
Dominika Tkaczyk <d.tkaczyk@gmail.com>
2019-04-26 06:18:45 +0100

    use phusion/passenger-full, uwsgi and nginx

--- rorapi/settings.py
+++ rorapi/settings.py
@@ -106,8 +106,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 ES = {
     'HOSTS': [{'host': os.environ.get('ELASTIC_HOST', 'localhost'),
