f91a8cfb431c540fa8789f145fc7048e5bfd0da2
Martin Fenner <martin.fenner@datacite.org>
2019-05-01 19:15:01 +0200

    secret_key via environment

--- rorapi/settings.py
+++ rorapi/settings.py
@@ -37,6 +37,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '0y0zn=hnz99$+c6lejml@chch54s2y2@-z##i$pstn62doft_g'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
