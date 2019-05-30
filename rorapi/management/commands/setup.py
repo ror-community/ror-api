2502f159a391dc5b19cbbb5f8dd36666de1df88c
Martin Fenner <martin.fenner@datacite.org>
2019-05-30 09:38:56 +0200

    only run setup scripts if ror json file exists. #39

--- rorapi/management/commands/setup.py
+++ rorapi/management/commands/setup.py
@@ -1,13 +1,22 @@
+import zipfile
+
 from django.core.management.base import BaseCommand
 from .deleteindex import Command as DeleteIndexCommand
 from .createindex import Command as CreateIndexCommand
 from .indexgrid import Command as IndexGridCommand
+from rorapi.settings import GRID
 
 
 class Command(BaseCommand):
     help = 'Setup ROR API'
 
     def handle(self, *args, **options):
+        # make sure ROR JSON file exists
+        if not zipfile.is_zipfile(GRID['ROR_ZIP_PATH']):
+            self.stdout.write('ROR dataset for GRID version {} not found. Please run the upgrade command first.'
+                    .format(GRID['VERSION']))
+            return
+
         DeleteIndexCommand().handle(args, options)
         CreateIndexCommand().handle(args, options)
         IndexGridCommand().handle(args, options)
