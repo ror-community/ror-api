f657d2685ce9752d2d572c0fd11102b0d9a2e2fe
Martin Fenner <martin.fenner@datacite.org>
2019-05-30 13:31:49 +0200

    support CORS. #40

--- rorapi/settings.py
+++ rorapi/settings.py
@@ -70,11 +70,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'coreapi',
     'django_prometheus',
+    'corsheaders',
     'rorapi',
 ]
 
 MIDDLEWARE = [
     'django_prometheus.middleware.PrometheusBeforeMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,6 +134,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
