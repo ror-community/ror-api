31b4ed8247be7bc4c77f9c454931d41c385ae613
Sarala Wimalaratne <sarala.dissanayake@gmail.com>
2019-09-20 12:07:03 +0100

    Revert "status filed added"
    This reverts commit d50b8b1d535f0ec1ba53d1756e37ead0c2656b8a.

--- rorapi/settings.py
+++ rorapi/settings.py
@@ -171,13 +171,8 @@ ES = Elasticsearch([{
 #     'URL': 'https://digitalscience.figshare.com/ndownloader/files/14399291'
 # }
 
-#GRID = {
-#    'VERSION': '2019-05-06',
-#    'URL': 'https://digitalscience.figshare.com/ndownloader/files/15167609'
-#}
-
 GRID = {
-    'VERSION': '2019-09-19',
+    'VERSION': '2019-05-06',
     'URL': 'https://digitalscience.figshare.com/ndownloader/files/15167609'
 }
 
