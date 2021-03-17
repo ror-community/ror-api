8fe05f2022217b07355ecbbe071a50cb79242cce
Esha Datta <edatta@crossref.org>
2021-03-17 12:23:23 -0400

    rebuilding ror data with all attributes and correct ids

--- rorapi/settings.py
+++ rorapi/settings.py
@@ -210,7 +210,7 @@ GRID['DIR'] = os.path.join(BASE_DIR, 'rorapi', 'data',
 GRID['GRID_ZIP_PATH'] = os.path.join(GRID['DIR'], 'grid.zip')
 GRID['GRID_JSON_PATH'] = os.path.join(GRID['DIR'], 'grid.json')
 
-ROR_DUMP = {'VERSION': '2020-12-23'}
+ROR_DUMP = {'VERSION': '2021-03-17'}
 
 ROR_DUMP['DIR'] = os.path.join(BASE_DIR, 'rorapi', 'data',
                                'ror-{}'.format(ROR_DUMP['VERSION']))
