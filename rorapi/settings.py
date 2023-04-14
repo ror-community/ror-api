73b6d5ce0d31a4b546baaabc144668875ec8bd31
lizkrznarich <liz.krznarich@datacite.org>
2023-04-14 14:13:22 -0500

    allow indexing 2 ES instances

--- rorapi/settings.py
+++ rorapi/settings.py
@@ -130,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 ES_VARS = {
     'INDEX': 'organizations',
     'INDEX_TEMPLATE': os.path.join(BASE_DIR, 'rorapi', 'index_template.json'),
+    'INDEX_TEMPLATE_ES7': os.path.join(BASE_DIR, 'rorapi', 'index_template_es7.json'),
     'BATCH_SIZE': 20,
     'MAX_PAGE': 500,  # = <ES LIMIT 10000> / BATCH_SIZE
     'BULK_SIZE': 500
@@ -146,6 +147,15 @@ else:
 ES = Elasticsearch([{
     'host': os.environ.get('ELASTIC_HOST', 'elasticsearch'),
     'port': int(os.environ.get('ELASTIC_PORT', '9200'))
+}],
+    http_auth=http_auth,
+    use_ssl=False,
+    timeout=240,
+    connection_class=RequestsHttpConnection)
+
+ES7 = Elasticsearch([{
+    'host': os.environ.get('ELASTIC7_HOST', 'elasticsearch7'),
+    'port': int(os.environ.get('ELASTIC7_PORT', '9201'))
 }],
     http_auth=http_auth,
     use_ssl=False,
