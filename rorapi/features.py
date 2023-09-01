deaf713bb55a23fc78702a9d541623c03476453b
lizkrznarich <liz.krznarich@datacite.org>
2023-09-01 14:57:38 -0500

    code and tests for v2 api; code refactor for multiple versions

--- rorapi/features.py
+++
@@ -1,6 +0,0 @@
-import ldclient
-from ldclient.config import Config
-from .settings import LAUNCH_DARKLY_KEY
-
-ldclient.set_config(Config(LAUNCH_DARKLY_KEY))
-launch_darkly_client = ldclient.get()
--- rorapi/management/commands/setup.py
+++ rorapi/management/commands/setup.py
@@ -2,16 +2,19 @@ import requests
 import zipfile
 
 from django.core.management.base import BaseCommand
-from .deleteindex import Command as DeleteIndexCommand
-from .createindex import Command as CreateIndexCommand
-from .indexrordump import Command as IndexRorDumpCommand
+from rorapi.management.commands.deleteindex import Command as DeleteIndexCommand
+from rorapi.management.commands.createindex import Command as CreateIndexCommand
+from rorapi.management.commands.indexrordump import Command as IndexRorDumpCommand
 from rorapi.settings import ROR_DUMP
 
 HEADERS = {'Accept': 'application/vnd.github.v3+json'}
 
-def get_ror_dump_sha(filename):
+def get_ror_dump_sha(filename, use_test_data):
     sha = ''
-    contents_url = ROR_DUMP['REPO_URL'] + '/contents'
+    if use_test_data:
+        contents_url = ROR_DUMP['TEST_REPO_URL'] + '/contents'
+    else:
+        contents_url = ROR_DUMP['PROD_REPO_URL'] + '/contents'
     try:
         response = requests.get(contents_url, headers=HEADERS)
     except requests.exceptions.RequestException as e:
@@ -30,11 +33,19 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('filename', type=str, help='Name of data dump zip file to index without extension')
+        parser.add_argument('-s', '--schema', type=int, choices=[1, 2], help='Schema version to index if only indexing 1 version. Only set if not indexing both versions.')
+        parser.add_argument('-t', '--testdata', action='store_true', help='Set flag to pull data dump from ror-data-test instead of ror-data')
 
     def handle(self, *args, **options):
         # make sure ROR dump file exists
         filename = options['filename']
-        sha = get_ror_dump_sha(filename)
+        use_test_data = options['testdata']
+        if use_test_data:
+            print("Using ror-data-test repo")
+        else:
+            print("Using ror-data repo")
+
+        sha = get_ror_dump_sha(filename, use_test_data)
 
         if sha:
             DeleteIndexCommand().handle(*args, **options)
@@ -42,7 +53,7 @@ class Command(BaseCommand):
             IndexRorDumpCommand().handle(*args, **options)
 
         else:
-            self.stdout.write('ROR dataset for version {} not found. '.
+            self.stdout.write('ROR dataset for file {} not found. '.
                               format(filename) +
                               'Please generate the data dump first.')
             return
