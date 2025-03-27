d3d44ea69c50ce10b431fe78ccb986081e6cfa44
Ashwini Sukale <ashwini.sukale@datacite.org>
2025-03-27 08:59:57 +0530

    Rate limit part will be done next release

--- rorapi/settings.py
+++ rorapi/settings.py
@@ -72,8 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_prometheus.middleware.PrometheusAfterMiddleware',
-    'rorapi.middleware.ClientRateLimitMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware'
 ]
 
 ROOT_URLCONF = 'rorapi.common.urls'
