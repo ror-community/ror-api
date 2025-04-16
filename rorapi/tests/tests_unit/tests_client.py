8ac04575be01f05c44c775e6a6882973080af289
lizkrznarich <liz.krznarich@datacite.org>
2025-04-16 13:09:26 -0500

    remove unneeded test

--- rorapi/tests/tests_unit/tests_client.py
+++ rorapi/tests/tests_unit/tests_client.py
@@ -5,7 +5,3 @@ class ClientTests(TestCase):
     def test_client_registration(self):
         client = Client.objects.create(email='test@example.com')
         self.assertIsNotNone(client.client_id)
-
-    def test_rate_limiting(self):
-        response = self.client.get('/client-id/', HTTP_CLIENT_ID="INVALID_ID")
-        self.assertEqual(response.status_code, 429)
