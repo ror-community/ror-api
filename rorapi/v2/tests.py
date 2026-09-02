def3d8f92561dced87f0a09e3d2466af3ed52bd3
jrhoads <jrhoads@datacite.org>
2026-09-02 17:02:15 +0200

    test: update Client import and validation endpoint test

--- rorapi/v2/tests.py
+++ rorapi/v2/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
-from .models.client import Client
+from rorapi.v2.models import Client
 
 class ClientTests(TestCase):
     def test_client_registration(self):
         client = Client.objects.create(email='test@example.com')
         self.assertIsNotNone(client.client_id)
 
-    def test_rate_limiting(self):
-        response = self.client.get('/client-id/', HTTP_CLIENT_ID="INVALID_ID")
-        self.assertEqual(response.status_code, 429)
+    def test_validate_client_id(self):
+        response = self.client.get('/validate-client-id/INVALID_ID/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.json()['valid'])
