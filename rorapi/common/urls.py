9120c7194abaf92be1d38af238769fd6405978aa
Joseph Rhoads <jrhoads@datacite.org>
2026-09-16 12:47:51 +0200

    Upgrade Django to 3.2 and update dependencies (#546)

--- rorapi/common/urls.py
+++ rorapi/common/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url, include
-from django.urls import path, re_path
+from django.urls import include, re_path
 from rest_framework.documentation import include_docs_urls
 from  . import views
 from rorapi.common.views import (
@@ -7,21 +6,21 @@ from rorapi.common.views import (
 
 urlpatterns = [
     # Health check
-    url(r"^(?P<version>v2)\/heartbeat$", HeartbeatView.as_view()),
-    url(r"^heartbeat$", HeartbeatView.as_view()),
+    re_path(r"^(?P<version>v2)\/heartbeat$", HeartbeatView.as_view()),
+    re_path(r"^heartbeat$", HeartbeatView.as_view()),
     # Using REST API
-    url(r"^(?P<version>v2)\/generateaddress\/(?P<geonamesid>[0-9]+)", GenerateAddress.as_view()),
-    path('generateaddress/<str:geonamesid>', GenerateAddress.as_view()),
-    url(r"^generateid$", GenerateId.as_view()),
+    re_path(r"^(?P<version>v2)\/generateaddress\/(?P<geonamesid>[0-9]+)", GenerateAddress.as_view()),
+    re_path(r"^generateaddress\/(?P<geonamesid>[^/]+)$", GenerateAddress.as_view()),
+    re_path(r"^generateid$", GenerateId.as_view()),
     re_path(r"^(?P<version>v2)\/bulkupdate$", BulkUpdate.as_view()),
     re_path(r"^(?P<version>v2)\/register$", ClientRegistrationView.as_view()),
-    path('validate-client-id/<str:client_id>/', ValidateClientView.as_view()),
-    url(r"^(?P<version>v2)\/indexdata/(?P<branch>.*)", IndexData.as_view()),
-    url(r"^(?P<version>v2)\/indexdatadump\/(?P<filename>v(\d+\.)?(\d+\.)?(\*|\d+)-\d{4}-\d{2}-\d{2}-ror-data)\/(?P<dataenv>(test|prod))$", IndexDataDump.as_view()),
-    url(r"^(?P<version>v2)\/", include(views.organizations_router.urls)),
-    url(r"^", include(views.organizations_router.urls)),
-    url(r"^docs/", include_docs_urls(title="Research Organization Registry")),
+    re_path(r"^validate-client-id\/(?P<client_id>[^/]+)\/$", ValidateClientView.as_view()),
+    re_path(r"^(?P<version>v2)\/indexdata/(?P<branch>.*)", IndexData.as_view()),
+    re_path(r"^(?P<version>v2)\/indexdatadump\/(?P<filename>v(\d+\.)?(\d+\.)?(\*|\d+)-\d{4}-\d{2}-\d{2}-ror-data)\/(?P<dataenv>(test|prod))$", IndexDataDump.as_view()),
+    re_path(r"^(?P<version>v2)\/", include(views.organizations_router.urls)),
+    re_path(r"^", include(views.organizations_router.urls)),
+    re_path(r"^docs/", include_docs_urls(title="Research Organization Registry")),
     # Prometheus
-    url("", include("django_prometheus.urls")),
+    re_path("", include("django_prometheus.urls")),
 
 ]
